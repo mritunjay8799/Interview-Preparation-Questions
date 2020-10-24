e8e15afe4563a7f5f862d0e965d985e26f54b2d0
Mritunjay Tripathi <36035989+mritunjay8799@users.noreply.github.com>
2020-10-25 00:06:18 +0530

    Create Adjacency List.cpp

---
+++ Graph/Adjacency List.cpp
@@ -0,0 +1,31 @@
+======================================================================          PRINT ADJACENCY LIST        =======================================================================
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printGraph(vector<int> adj[], int V)
+{
+    for (int v = 0; v < V; v++) 
+    { 
+        cout << v; 
+        for (auto x : adj[v]) 
+           cout << "-> " << x; 
+        cout<<endl;
+    } 
+}
+
+
+int main()
+{
+  int n,e;
+  cin>>n>>e;
+  int a,b;
+  vector<int>adj[n];
+  for(int i = 0; i<e; i++)
+  {
+    cin>>a>>b;
+    adj[a],push_back(b);
+    adj[b],push_back(a);
+  }
+  printGraph(adj,n);
+}
