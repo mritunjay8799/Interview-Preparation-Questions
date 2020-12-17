026aca2ec2a682bf8b839296bd3c4a91264d9c8d
Mritunjay Tripathi <36035989+mritunjay8799@users.noreply.github.com>
2020-12-17 20:29:06 +0530

    Update 3. Level Order Traversal.cpp

--- Tree/3. Level Order Traversal.cpp
+++ Tree/3. Level Order Traversal.cpp
@@ -28,3 +28,64 @@ vector<int> levelOrder(Node* node)
   }
   return v;
 }
+=================================================================================================================================================================================
+ 
+ When a 2-D vector has to return - After every level, put a NULL and use it as separator for different levels.
+ --------------------------------
+    
+    class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(root==NULL)
+            return {};
+       vector<vector<int> >res;
+        queue<TreeNode*>q;
+        q.push(root);
+        q.push(NULL);
+        while(q.size()!=1)
+        {
+            vector<int>temp;
+            while(q.front()!=NULL)
+            {
+                auto a = q.front();
+                temp.push_back(a->val);
+                q.pop();
+                if(a->left!=NULL)
+                    q.push(a->left);
+                if(a->right!=NULL)
+                    q.push(a->right);
+            }
+            q.push(NULL);
+            q.pop();
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
+ 
+-----------------------------------------------------------------OR YOU CAN USE SIZE OF QUEUE AS SEPARATOR --------------------------------------------------------------------
+
+ if(root==NULL)
+            return {};
+        vector<vector<int> >res;
+        queue<TreeNode*>q;
+        vector<int>v;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n = q.size();
+            while(n)
+            {
+                TreeNode*ptr = q.front();
+                q.pop();
+                v.push_back(ptr->val);
+                if(ptr->left)
+                    q.push(ptr->left);
+                if(ptr->right)
+                    q.push(ptr->right);
+                n--;
+            }
+            res.push_back(v);
+            v.clear();
+        }
+        return res;
