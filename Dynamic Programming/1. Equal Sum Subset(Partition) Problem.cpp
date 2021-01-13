cf879832993557e171e91ca1f63ca5c6172a2da2
Mritunjay Tripathi <36035989+mritunjay8799@users.noreply.github.com>
2021-01-13 16:52:21 +0530

    Update 1. Equal Sum Subset(Partition) Problem.cpp

--- Dynamic Programming/1. Equal Sum Subset(Partition) Problem.cpp
+++ Dynamic Programming/1. Equal Sum Subset(Partition) Problem.cpp
@@ -66,3 +66,37 @@ public:
 
 TABULAR METHOD CODE-
 --------------------
+
+int dp[201][200001];
+class Solution {
+public:
+    int solve(vector<int>nums, int n, int sum)
+    {
+        int i,j;
+        for(i = 1;i<n+1;i++)
+        {
+            for(j = 1;j<sum+1;j++)
+            {
+                if(nums[i-1] <= j)
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][sum];
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        for(int i= 0; i<n;i++)
+            sum+=nums[i];
+        if(sum%2!=0)
+            return false;
+        sum = sum/2;
+        for(int i = 0;i<sum+1;i++)
+            dp[0][i] = 0;
+        for(int i = 0;i<n+1;i++)
+            dp[i][0] = 1;
+        return solve(nums,n,sum);
+    }
+};    
